c53aa8df318e67aaaf70b426039d06176bba998a
Gabriel <gabrielroodriz@gmail.com>
2020-03-13 13:07:12 -0300

    Fixing(authController): Incluindo um token na aplicação

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const User = require("../models/User");
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
+
+const jwt = require("jsonwebtoken");
+
+const authConfig = require('../config/auth');
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
   try {
-    if (await User.findOne({
-        type: 'email'
-      })) {
+    if (
+      await User.findOne({
+        type: "email"
+      })
+    ) {
       return res.status(400).send({
         error: "User already exists"
       });
@@ -28,7 +34,7 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post('/authenticate', async (req, res) => {
+router.post("/authenticate", async (req, res) => {
   const {
     email,
     password
@@ -36,24 +42,30 @@ router.post('/authenticate', async (req, res) => {
 
   const user = await User.findOne({
     email
-  }).select('+password');
+  }).select("+password");
 
   if (!user) {
     return res.status(400).send({
-      erro: 'User not found'
-    })
+      erro: "User not found"
+    });
   }
-  if (!await bcrypt.compare(password, user.password)) {
+  if (!(await bcrypt.compare(password, user.password))) {
     return res.status(400).send({
       error: "Invalid password"
     });
   }
   user.password = undefined;
 
-  res.send({
-    user
+  const token = jwt.sign({
+    id: user.id
+  }, authConfig.secret,{
+    expiresIn: 86400,
   });
 
-})
+  res.send({
+    user,
+    token
+  });
+});
 
 module.exports = app => app.use("/auth", router);
