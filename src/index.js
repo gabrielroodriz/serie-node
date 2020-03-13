6c2696e1d2268218bac0a5ce604b3836c4ecbb2b
Gabriel <gabrielroodriz@gmail.com>
2020-03-13 10:48:02 -0300

    Fixing(Back): Iniciando projeto

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.listen(PORT, () => {
+    console.log(`Servidor sendo executado na PORT: ${PORT}`)
+});
