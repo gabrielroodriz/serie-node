f7541bd78edb7e38cbb6c17a2b0b1b10ae31cf37
Gabriel <gabrielroodriz@gmail.com>
2020-03-13 11:47:41 -0300

    Fixing(authController): Removendo a senha de resposta user e impedindo a inseção do mesmo user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,20 +1,26 @@
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-    try {
-        const user = await User.create(req.body);
-
-        return res.send({
-            user
-        });
-    } catch (err) {
-        return res.status(400).send({
-            error: 'Registration failed'
-        });
+router.post("/register", async (req, res) => {
+  try {
+    if (await User.findOne({type: 'email' })) {
+      return res.status(400).send({ error: "User already exists" });
     }
+
+    const user = await User.create(req.body);
+
+    user.password = undefined;
+
+    return res.send({
+      user
+    });
+  } catch (err) {
+    return res.status(400).send({
+      error: "Registration failed"
+    });
+  }
 });
 
-module.exports = app => app.use('/auth', router);
+module.exports = app => app.use("/auth", router);
