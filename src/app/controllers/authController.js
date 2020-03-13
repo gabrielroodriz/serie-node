afd8a88697442421c7cbd3990f3f1b9a999b8176
Gabriel <gabrielroodriz@gmail.com>
2020-03-13 17:15:36 -0300

    Fixing(authController): Implatado a funcionalidade de reset de senha

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -113,7 +113,6 @@ router.post("/forgot_password", async (req, res) => {
       },
     }, (err) => {
       if (err) {
-        console.log(err)
         return res.status(400).send({
           error: 'Cannot send forgot password email'
         });
@@ -122,9 +121,48 @@ router.post("/forgot_password", async (req, res) => {
       return res.send();
     })
   } catch (error) {
-    console.log(error)
     res.status(400).send({
       error: "Erro on forgot password, try again"
+    });
+  }
+});
+
+router.post('/reset_password', async (req, res) => {
+  const {
+    email,
+    token,
+    password
+  } = req.body;
+
+  try {
+    const user = await User.findOne({
+      email
+    }).select('+passwordResetToken passwordResetExpires');
+
+    if (!user) {
+      return res.status(400).send({
+        error: 'User not found'
+      });
+    }
+    if (token !== user.passwordResetToken)
+      return res.status(400).send({
+        error: 'Token invalid, try agin'
+      });
+    const now = new Date();
+    if (now > user.passwordResetExpires)
+      return res.status(400).send({
+        error: 'Token expired, generate a new one'
+      })
+
+    user.password = password;
+
+    await user.save();
+
+    res.send();
+  } catch (error) {
+    console.log(error)
+    res.status(400).send({
+      error: 'Cannot reset password, try again'
     });
   }
 });
